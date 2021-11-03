1410d04eef3a34ad431213e4434e9bf193323b83
kadd0ury <kaddoury2017@gmail.com>
2021-11-03 15:19:31 +0100

    fixing demoClass

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,7 +13,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication  extends SpringBootServletInitializer{
+public class DemoApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
